b5ac24b0b40364aceda35a36309053bf33490a39
TimofeyFilkin <115225243+TimofeyFilkin@users.noreply.github.com>
2024-06-28 21:23:33 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+Blocks.payloadSource.buildVisibility = BuildVisibility.shown;
+Blocks.itemSource.buildVisibility = BuildVisibility.shown;
+Blocks.powerSource.buildVisibility = BuildVisibility.shown;
+Blocks.payloadVoid.buildVisibility = BuildVisibility.shown;
+Blocks.itemVoid.buildVisibility = BuildVisibility.shown;
+Blocks.powerVoid.buildVisibility = BuildVisibility.shown;
+Blocks.liquidSource.buildVisibility = BuildVisibility.shown;
+Blocks.liquidVoid.buildVisibility = BuildVisibility.shown;
+Blocks.heatSource.buildVisibility = BuildVisibility.shown;
